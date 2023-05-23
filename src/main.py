c41db0c18899f2a97044740be832b79a0e441bcc
jkmg <jcmguga@gmail.com>
2023-05-23 14:42:53 +0200

    stdrd

--- src/main.py
+++ src/main.py
@@ -1,56 +1,173 @@
-from sklearn.preprocessing import PolynomialFeatures
 import streamlit as st
+import st_aggrid
+from st_aggrid import AgGrid, GridOptionsBuilder, JsCode
+from st_aggrid.shared import GridUpdateMode
 import pandas as pd
 import numpy as np
-import joblib
-from joblib import load
-#from pycaret.regression import *
 
-st.write(""" ### Determination of STD NMR Binding Epitopes using the Reduced Dataset Approach from the STD factors at 0.75 and 6 s """)
+st.write(""" ## Determination of STD NMR Binding Epitopes using the Reduced Dataset Approach from STD factors at 2 saturation times """)
 
-st.sidebar.header('Analytical Approach')
+st.sidebar.header('CSV File Upload Option')
 
 # Collects user input features into dataframe
-uploaded_file1 = st.sidebar.file_uploader("Upload a CSV File", type=["csv"])
+uploaded_file1 = st.sidebar.file_uploader("**You can also upload a CSV File (without column headers) containing 3 columns, the first with the proton names, then the experimental STD factors at short (typically 0.5, 0.75 or 1 s; 0.75 s is recommended) and large (typically 6 or 8 s) saturation times**", type=["csv"])
+tsat_short1 = st.sidebar.number_input("**Enter the Short Saturation Time, in seconds**", value = 0.75, min_value=0.50, max_value=1.00)
 if uploaded_file1 is not None:
-    df = pd.read_csv(uploaded_file1, header = None)
-    df.columns=['Proton', 'STD_0p75s', 'STD_6s']
-    df['ksat'] = (- np.log((df.STD_6s - df.STD_0p75s)/df.STD_6s))/0.75
-    df['STD0'] = df.ksat*df.STD_6s
-    df['Epitope'] = df.STD0/(np.max(df.STD0))*100
-    df.drop(['STD_0p75s', 'STD_6s'], inplace=True, axis=1)
+    df1 = pd.read_csv(uploaded_file1, header = None)
+    df1.columns=['Proton', 'STD_short_tsat', 'STD_long_tsat']
+    df1['ksat'] = (- np.log((df1.STD_long_tsat - df1.STD_short_tsat)/df1.STD_long_tsat))/tsat_short1
+    df1['STD0'] = df1.ksat*df1.STD_long_tsat
+    df1['Epitope'] = df1.STD0/(np.max(df1.STD0))*100
+    df1.drop(['STD_short_tsat', 'STD_long_tsat'], inplace=True, axis=1)
+    # Function
+    @st.experimental_memo
+    def convert_df1(data1): 
+        "Converts the data to a CSV format"
+        return data1.to_csv(index=False).encode('utf-8')
 
-# Displays the user input features
-st.subheader('Analytical Approach')
+    st.subheader("Download Results")
+    col1,col2 = st.columns(2)
+    csv = convert_df1(df1)
+    col1.write("Save locally")
+    col1.download_button(
+    "Press to Download 🗳️",
+    csv,
+    "STD0_Reduced_Dataset.csv",
+    "text/csv",
+    key='download-csv'
+    )
+
+else:
+    # Create an AgGrid table from a pandas DataFrame
+    tsat_short = st.number_input("**Enter the Short Saturation Time Employed, in seconds**", value = 0.75, min_value=0.50, max_value=1.00)
+    d = {'Proton_Name': [""],'STD_short_tsat': [np.nan],'STD_long_tsat': [np.nan]}
+    df = pd.DataFrame(data = d)
+    # Display the Dataframe in AgGrid
+    # JavaScript function to add a new row to the AgGrid table
+    js_add_row = JsCode("""
+    function(e) {
+        let api = e.api;
+        let rowPos = e.rowIndex + 1; 
+        api.applyTransaction({addIndex: rowPos, add: [{}]})    
+    };
+    """     
+    )
+
+    # Cell renderer for the '🔧' column to render a button
+    cellRenderer_addButton = JsCode('''
+        class BtnCellRenderer {
+            init(params) {
+                this.params = params;
+                this.eGui = document.createElement('div');
+                this.eGui.innerHTML = `
+                <span>
+                    <style>
+                    .btn_add {
+                        background-color: #71DC87;
+                        border: 2px solid black;
+                        color: #D05732;
+                        text-align: center;
+                        display: inline-block;
+                        font-size: 12px;
+                        font-weight: bold;
+                        height: 2em;
+                        width: 10em;
+                        border-radius: 12px;
+                        padding: 0px;
+                    }
+                    </style>
+                    <button id='click-button' 
+                        class="btn_add" 
+                        >&#x2193; Add</button>
+                </span>
+            `;
+            }
+            getGui() {
+                return this.eGui;
+            }
+        };
+        ''')
+
+    # Create a GridOptionsBuilder object from our DataFrame
+    gd = GridOptionsBuilder.from_dataframe(df)
+    #gd_results = GridOptionsBuilder.from_dataframe(results)
+
+    # Configure the default column to be editable
+    # sets the editable option to True for all columns
+    gd.configure_default_column(editable=True)
+    #gd_results.configure_default_column(editable=True)
+
+    # Configure the '🔧' column to use our the cell renderer 
+    # and onCellClicked function
+    gd.configure_column( field = '🔧', 
+                        onCellClicked = js_add_row,
+                        cellRenderer = cellRenderer_addButton
+                        )
+    #gd_results.configure_column( field = 'Results', 
+    #                    onCellClicked = js_add_row
+    #                    )
+    gridoptions = gd.build()
+    #gridoptions = gd_results.build()
+
+    # AgGrid Table with Button Feature
+    # Streamlit Form helps from rerunning on every widget-click
+    # Also helps in providing layout
+
+    with st.form('STD NMR Reduced Dataset Approach') as f:
+        st.subheader(""" Complete the table below with your Ligand Proton Names and STD factors :writing_hand: """)
+        
+    # Inside the form, we are displaying an AgGrid table using the AgGrid function. 
+    # The allow_unsafe_jscode parameter is set to True, 
+    # which allows us to use JavaScript code in the AgGrid configuration
+    # The theme parameter is set to 'balham', 
+    # which applies the Balham theme to the table
+    # The height parameter is set to 200, 
+    # which specifies the height of the table in pixels.
+    # The fit_columns_on_grid_load parameter is set to True, 
+    # which ensures that the columns of the table are resized to fit 
+    # the width of the table when it is first displayed
+
+        response = AgGrid(df,
+                        gridOptions = gridoptions, 
+                        editable=True,
+                        allow_unsafe_jscode = True, 
+                        theme = 'balham',
+                        height = 200,
+                        fit_columns_on_grid_load = True)
+        st.write(" *Note: Don't forget to hit enter ↩ on new entry.*")
+        st.form_submit_button("Confirm item(s) 🔒", type="primary")
+
+    # Visualize the AgGrid when submit button triggered           
+    st.subheader("Updated Table")
+    # Fetch the data from the AgGrid Table
+    res = response['data'] 
+    res['STD_long_tsat'] = res['STD_long_tsat'].astype(float)
+    res['STD_short_tsat'] = res['STD_short_tsat'].astype(float)
+    res['ksat'] = (- np.log((res.STD_long_tsat - res.STD_short_tsat)/res.STD_long_tsat))/tsat_short
+    res['STD0'] = res.ksat*res.STD_long_tsat
+    res['Epitope'] = res.STD0/(np.max(res.STD0))*100
+    #res.drop(['STD_short_tsat', 'STD_long_tsat'], inplace=True, axis=1)
+    st.table(res)
+
+    # Function
+    @st.experimental_memo
+    def convert_df(data2): 
+        "Converts the data to a CSV format"
+        return data2.to_csv(index=False).encode('utf-8')
+
+    st.subheader("Download Results")
+    col1,col2 = st.columns(2)
+    csv = convert_df(response['data'])
+    col1.write("Save locally")
+    col1.download_button(
+    "Press to Download Results 🗳️",
+    csv,
+    "file.csv",
+    "text/csv",
+    key='download-csv'
+    )
 
 if uploaded_file1 is not None:
-    st.write(df)
+    st.write(df1)
 else:
     st.write('Awaiting CSV file to be uploaded')
-
-#st.sidebar.header('ML Approach')
-
-# Collects user input features into dataframe
-#uploaded_file2 = st.sidebar.file_uploader("Upload a CSV File (without column headers) containing 3 columns, the first with the proton names, then the experimental STD factors at 0.75s and finally the STD factros at 6s", type=["csv"])
-#if uploaded_file2 is not None:
-#    poly = joblib.load('/home/usuario/Documentos/Marie_Curie/Proyectos/STD_ML/Model_Deployment/polynomial_features_stdml_model')
-#    model=joblib.load("/home/usuario/Documentos/Marie_Curie/Proyectos/STD_ML/Model_Deployment/extra_trees_best_sklearn_regressor")
-#    df2 = pd.read_csv(uploaded_file2, header = None)
-#    df2.rename(columns={0: 'Proton', 1: 'STD_0p75s', 2:'STD_6s'}, inplace=True)
-#    df2["feature1"] = df2.STD_0p75s/df2.STD_6s
-#    df2["feature2"] = np.exp(df2.STD_0p75s/df2.STD_6s)
-#    dfval_poly = poly.transform(df2.loc[:,"STD_0p75s":"feature2"])
-#    dfval_poly = pd.DataFrame(dfval_poly)
-#    pred = model.predict(dfval_poly)
-#    pred = pd.DataFrame(pred, columns=['STD0_ML'])
-#    pred['Proton'] = df2.Proton.values
-#    pred = pred.reindex(columns=['Proton', 'STD0_ML'])
-#    pred['Epitope_ML'] = pred.STD0_ML/(np.max(pred.STD0_ML))*100
-
-# Displays the user input features
-#st.subheader('ML Approach')
-
-#if uploaded_file2 is not None:
-#    st.write(pred)
-#else:
-#    st.write('Awaiting CSV file to be uploaded. Currently using example input parameters (shown below).')
