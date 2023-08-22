c1291987e628ee843f7f4c4d1c29201e768796b5
jkmg <jcmguga@gmail.com>
2023-08-22 13:59:04 +0200

    Update main.py

--- src/main.py
+++ src/main.py
@@ -9,7 +9,7 @@ import numpy as np
 import yaml
 from yaml.loader import SafeLoader
 
-with open('stdrd/src/authentication.yaml') as file:
+with open('/mount/stdrd/src/authentication.yaml') as file:
     config = yaml.load(file, Loader=SafeLoader)
 
 authenticator = stauth.Authenticate(
