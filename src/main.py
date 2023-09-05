84fb927c5fe5af887465d2de2a0f4f7e77bb47b8
jkmg <jcmguga@gmail.com>
2023-09-05 13:50:27 +0200

    stdrd

--- src/main.py
+++ src/main.py
@@ -9,7 +9,7 @@ import numpy as np
 import yaml
 from yaml.loader import SafeLoader
 
-with open('/mount/src/stdrd/src/no_authentication.yaml') as file:
+with open('/mount/src/stdrd/src/noauthentication.yaml') as file:
     config = yaml.load(file, Loader=SafeLoader)
 
 authenticator = stauth.Authenticate(
